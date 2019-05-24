67f6a1549d2b5239ecb7a35a13952aec7700de41
xhzGitHub <739809951@qq.com>
2019-05-24 16:56:00 +0800

    0524 init

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="container"
+    style="max-width: 375px; margin: 0 auto; border: 1px solid gray;"
+  >
+    <Nav />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Nav from './partials/nav.vue'
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
---
+++ layouts/partials/nav.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="">
+    <header>
+      <nuxt-link to="/" class="logo">
+        Nuxty
+      </nuxt-link>
+      <nav>
+        <ul>
+          <li><nuxt-link to="/">Home</nuxt-link></li>
+          <li><nuxt-link to="user">user</nuxt-link></li>
+          <li><nuxt-link to="services">services</nuxt-link></li>
+          <li><nuxt-link to="contact">contact</nuxt-link></li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+  header {
+    background-color: rgb(0, 219, 146);
+    display: grid;
+    grid-template-colums: repeat(2, auto);
+  }
+</style>
